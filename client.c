4758426d2cad23ff82b46181f422f774b3293eb9
klow <patrick.guimaraes@laposte.net>
2014-12-29 12:15:18 +0100

    le client parle

--- client.c
+++ client.c
@@ -7,13 +7,14 @@
 
 void erreur(const char *msg)
 {
-    perror(msg);
-    exit(1);
+		perror(msg);
+		exit(1);
 }
 
 int main()
 {
-	int nbVoiture=0;
-		scanf("Commande de voiture\n 1 voiture :25$\n 10 voiture : 250000£ \n 10000000 voitures : payment nature",nbVoiture);
-	
+		int nbVoiture=0;
+		printf("Commande de voiture\n 1 voiture :25$\n 10 voiture : 250000£ \n 10000000 voitures : payment nature\n");
+		scanf("%d",&nbVoiture);
+		printf("%d nb voiture commander",nbVoiture);	
 }
